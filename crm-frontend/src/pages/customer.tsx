642137960e2efb80cc63ddaf2a1c04afa3e1ac76
denizderK <denizderya8668@gmail.com>
2023-12-19 20:32:19 +0300

    edt

--- crm-frontend/src/pages/customer.tsx
+++ crm-frontend/src/pages/customer.tsx
@@ -1,10 +1,26 @@
 import Menu from '@/components/menu'
-import React from 'react'
+import { AppDispatch, RootState } from '@/store'
+import { getUsers } from '@/store/apps/user'
+import React, { useEffect } from 'react'
+import { useSelector, useDispatch } from "react-redux"
 
 const Customer = () => {
+  //** redux */
+  const dispatch =useDispatch<AppDispatch>()
+  usersLoading = useSelector((state:RootState) => state.user.usersLoading)
+  const users:any[] = useSelector((state:RootState) => state.user.users)
+ 
+
+  
+
+  useEffect(() => {
+    dispatch(getUsers)
+  }, [dispatch])
+  
   return (
     <>
       <Menu />
+      {usersLoading  ? "yukleniyor": "yuklendi"}
     </>
   )
 }
